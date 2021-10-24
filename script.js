ce6df454ec2b6b12fecf57c78c7fae7ab5375ae2
Cadmo Júnior <43112108+cadmojr201@users.noreply.github.com>
2021-10-24 12:01:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const linksSocial = {
+  github: 'cadmojr201',
+  youtube: 'cadmojr201',
+  facebook: 'Oliveira.junior87',
+  instagram: 'cadmojr201',
+  twitter: 'cadmojr201'
+}
+
+function showMeSomething() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://www.${social}.com/${linksSocial[social]}`
+  }
+}
+
+function changeLinks() {
+  // document.getElementById('name')
+  // textContent = 'Dryhelly'
+}
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocial.github}`
+  //alert(url)
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      userPhoto.src = data.avatar_url
+      userName.textContent = data.name
+      linkGit.href = data.html_url
+      gitName.textContent = data.login
+      userBio.textContent = data.bio
+    })
+}
+
+showMeSomething()
+getGitHubProfileInfos()
